1fc1f0e01650881f9177c1a76a285b9ca205de66
Tom Barclay <tom@tombarclay.com>
2016-04-26 15:26:53 -0700

    cleanup

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import (print_function, division, absolute_import)
-
-import logging
-logging.basicConfig()  # Avoid "No handlers could be found for logger" warning
-logger = logging.getLogger(__name__)
-
